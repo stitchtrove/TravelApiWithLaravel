74199d9909a4b553526e81e72e1fe2c57787772d
Amy Westlake <amykatewestlake@gmail.com>
2023-06-12 17:46:27 +0100

    created TravelResource and Controller to return data when api endpoint visited

---
+++ app/Http/Resources/TravelResource.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TravelResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'slug'       => $this->slug,
+            'name'      => $this->name,
+            'description' => $this->description,
+            'no_of_days' => $this->no_of_days,
+            'no_of_nights' => $this->no_of_nights
+        ];
+    }
+}
--- app/Models/Travel.php
+++ app/Models/Travel.php
@@ -4,13 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Travel extends Model
 {
-    use HasFactory, Sluggable;
+    use HasFactory;
 
     protected $table = 'travels';
 
@@ -19,7 +18,7 @@ class Travel extends Model
         'slug',
         'name',
         'description',
-        'number_of_days',
+        'no_of_days',
     ];
 
     public function sluggable(): array
@@ -34,7 +33,7 @@ class Travel extends Model
     public function numberOfNights(): Attribute
     {
         return Attribute::make(
-            get: fn ($value, $attributes) => $attributes['number_of_days'] - 1
+            get: fn ($value, $attributes) => $attributes['no_of_days'] - 1
         );
     }
 
