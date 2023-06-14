14c753a7f81a09e228aad805be09f8220f708503
Amy Westlake <amykatewestlake@gmail.com>
2023-06-14 16:57:49 +0100

    added sorting results, validation rules and feature tests

--- app/Http/Controllers/Api/V1/ToursController.php
+++ app/Http/Controllers/Api/V1/ToursController.php
@@ -5,18 +5,25 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Tour;
+use Illuminate\Validation\Rule;
 use App\Models\Travel;
 use App\Http\Resources\TourResource;
+use App\Http\Requests\ToursListRequest;
 
 class ToursController extends Controller
 {
-    public function index(Travel $travel, Request $request)
+    public function index(Travel $travel, ToursListRequest $request)
     {
         $request->validate([
             'priceFrom' => 'numeric',
             'priceTo' => 'numeric',
             'dateFrom' => 'date',
-            'dateTo' => 'date'
+            'dateTo' => 'date',
+            'sortBy' => Rule::in(['price']),
+            'sortOrder' => Rule::in(['asc', 'desc']),
+        ], [
+            'sortBy' => "The 'sortBy' parameter accepts only 'price' value",
+            'sortOrder' => "The 'sortOrder' parameter accepts only 'asc' and 'desc' values",
         ]);
 
         $entries = $travel->tours()
@@ -31,6 +38,9 @@ class ToursController extends Controller
             })
             ->when($request->dateTo, function ($query) use ($request) {
                 $query->where('start_date', '<=', $request->dateTo);
+            })
+            ->when($request->sortBy, function ($query) use ($request) {
+                $query->orderBy($request->sortBy, $request->sortOrder);
             })
             ->orderBy('start_date', 'asc')
             ->paginate();
