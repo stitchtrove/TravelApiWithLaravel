f2b56492beb9ec4afb9a04f821949655b7adcc49
Amy Westlake <amykatewestlake@gmail.com>
2023-06-14 16:47:03 +0100

    added filters to tour api

--- app/Http/Controllers/Api/V1/ToursController.php
+++ app/Http/Controllers/Api/V1/ToursController.php
@@ -10,9 +10,30 @@ use App\Http\Resources\TourResource;
 
 class ToursController extends Controller
 {
-    public function index(Travel $travel)
+    public function index(Travel $travel, Request $request)
     {
-        $entries = $travel->tours()->orderBy('start_date')->paginate();
+        $request->validate([
+            'priceFrom' => 'numeric',
+            'priceTo' => 'numeric',
+            'dateFrom' => 'date',
+            'dateTo' => 'date'
+        ]);
+
+        $entries = $travel->tours()
+            ->when($request->priceFrom, function ($query) use ($request) {
+                $query->where('price', '>=', $request->priceFrom * 100);
+            })
+            ->when($request->priceTo, function ($query) use ($request) {
+                $query->where('price', '<=', $request->priceTo * 100);
+            })
+            ->when($request->dateFrom, function ($query) use ($request) {
+                $query->where('start_date', '>=', $request->dateFrom);
+            })
+            ->when($request->dateTo, function ($query) use ($request) {
+                $query->where('start_date', '<=', $request->dateTo);
+            })
+            ->orderBy('start_date', 'asc')
+            ->paginate();
 
         return TourResource::collection($entries);
     }
